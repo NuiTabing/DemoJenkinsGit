182807e4ba1f6b00cdc95c9f1cfc1646c154af63
Ubolwan Vorakanonta <nuitabing@gmail.com>
2022-10-04 11:38:03 -0700

    add new

---
+++ src/main/java/Demo/Jenkins/DemoForJenkins.java
@@ -0,0 +1,12 @@
+package Demo.Jenkins;
+
+public class DemoForJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
